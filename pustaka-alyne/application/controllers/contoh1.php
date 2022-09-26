2b12bdf47c468f253aa8bbe364766667dc56ae6e
rossalynecn <rossalyne17@gmail.com>
2022-09-26 19:07:14 +0700

    alyne

---
+++ pustaka-alyne/application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "<h2>Nama saya Rossalyne
+        Saya tinggal di daerah Ciledug dan
+        Saya suka tidur</h2>";
+    }
+}
---
+++ pustaka-alyne/application/views/view-latihan.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+     <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo! Selamat Bergabung bersama Alyne<br>
+    Nilai 66 = <?= $nilai1; ?>
+    Nilai 99 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
