57b019c3e017341e16de6f7fc2cea8c69df28b5c
anissapuspita08 <anissapsptsr@gmail.com>
2022-10-02 23:32:31 +0700

    anissa

---
+++ pustaka-booking-anissa/application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+class contoh1 extends CI_Controller
+{
+	public function index ()
+	{
+		echo "<h1> Perkenalkan </h1>";
+		echo "Nama saya Anissa Puspitasari 
+			   Saya Tinggal di daerah Ciledug
+			   Saya suka olahraga Bulu Tangkis";
+	}
+}
